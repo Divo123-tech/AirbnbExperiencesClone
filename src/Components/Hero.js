ff8e84e5300392a5185e7ad50ae88b6d7f6e9bbe
Divo123 <topherharsh@gmail.com>
2023-04-21 10:31:45 +0800

    my first commit

---
+++ src/Components/Hero.js
@@ -0,0 +1,21 @@
+import React from "react";
+import collage from "../photo-grid.png";
+import "./Hero.css"
+
+
+export default function Hero() {
+  return (
+    <div>
+      <div className="photo-container">
+        <img src={collage} alt="collage" className="image"/>
+      </div>
+      <div className="text-container">
+        <h1 className="hero-text title">Online Experiences</h1>
+        <p className="hero-text caption">
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../airbnb-logo.png"
+import "./Navbar.css"
+
+export default function Navbar(){
+    return(
+        <div className="navbar">
+            <img src={logo} alt="logo" width="120px" />
+        </div>
+    )
+}
